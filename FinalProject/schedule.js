13edc776c5107ff26e9551434307a190e731ffb2
Lvphucnguyen2k8 <70436761+Lvphucnguyen2k8@users.noreply.github.com>
2021-03-06 19:51:02 +0700

    ffeafaa

--- FinalProject/schedule.js
+++ FinalProject/schedule.js
@@ -55,36 +55,43 @@ let updateClock = function()
   {
     image = "https://wearecardinals.com/wp-content/uploads/2020/04/u1Re9qgMfM8d6kumlW85PS6s55jQh5fbdmppgQsP.jpeg";
     messageText = "Let's play sport!";
+    eventMessageJS.style.color = "white";
   }
   else if (time == wakeuptime)
   {
     image = "https://www.elegantthemes.com/blog/wp-content/uploads/2018/11/how-to-wake-up-early.png";
     messageText = "Wake up!";
+    eventMessageJS.style.color = "black";
   }
   else if (time == lunchtime)
   {
     image = "https://images-na.ssl-images-amazon.com/images/I/71pJu3A8EML._AC_SL1498_.jpg";
     messageText = "Let's have some lunch!";
+    eventMessageJS.style.color = "black";
   }
   else if (time == sleeptime)
   {
     image = "https://img.freepik.com/free-vector/little-boy-lying-bed-counting-sheep_74855-7582.jpg?size=338&ext=jpg";
     messageText = "Have a good sleep!";
+    eventMessageJS.style.color = "black";
   }
   else if (time < noon)
   {
     image = "https://simplifaster.com/wp-content/uploads/2017/05/Sunshine-Vitamin-D.jpg";
     messageText = "Good morning!";
+    eventMessageJS.style.color = "black";
   }
   else if (time >= evening)
   {
     image = "https://static01.nyt.com/images/2020/10/29/style/28MOON-01/oakImage-1603985177355-mediumSquareAt3X.jpg";
     messageText = "Good evening!";
+    eventMessageJS.style.color = "white";
   }
   else
   {
     image = "https://earthsky.org/upl/2013/09/sunrise-red-sea-Graham-Telford-e1489764712368.jpg";
     messageText = "Good afternoon!";
+    eventMessageJS.style.color = "white";
   }
 
   console.log(messageText); 
@@ -111,7 +118,7 @@ let sportEvent = function()
     }
     else
     {
-        sportTime = -1;
+        sporttime = -1;
         sportTimeButton.innerText = "Sport Time!";
         sportTimeButton.style.backgroundColor = "#222222";
     }
@@ -152,23 +159,10 @@ sleepTimeSelector.addEventListener("change", sleepEvent);
 
 function logout() {
 
-  let logout = "";
-  prompt("Ok to log out, Cancel to keep in your account");
-  if (logout == null) {
+  if (confirm("Ok to log out, Cancel to keep in your account")) {
 
-  break;
+    setTimeout(function () { alert("LOG OUT SUCCESSFUL!"); location.replace("login.html") }, 3000);
 
   }
-
-  else if (logout.length != 1) {
-
-      setTimeout(function () { alert("LOG OUT SUCCESSFUL!"); location.replace("login.html") }, 3000);
-
-  }
-
-  else {
-
-    break;
-
-  }
+  
 } 
