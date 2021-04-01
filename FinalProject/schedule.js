837bf4235545a934745ac675093e967e2cc1485a
Lvphucnguyen2k8 <70436761+Lvphucnguyen2k8@users.noreply.github.com>
2021-04-01 21:15:16 +0700

    huksaukgfkusa

--- FinalProject/schedule.js
+++ FinalProject/schedule.js
@@ -1,8 +1,5 @@
 let wakeuptime = 7;
 let noon = 12;
-let lunchtime = 12;
-let sleeptime = lunchtime + 2;
-let sporttime = -1;
 let evening = 18;
 
 let showCurrentTime = function()
@@ -44,49 +41,39 @@ let showCurrentTime = function()
 showCurrentTime();
 let updateClock = function() 
 {
-  //   let time = new Date().getHours();
-  //   let messageText = "hello";
-  //   let image = "https://wearecardinals.com/wp-content/uploads/2020/04/u1Re9qgMfM8d6kumlW85PS6s55jQh5fbdmppgQsP.jpeg";
+  let time = new Date().getHours();
+  let messageText = "hello";
+  let image = "https://wearecardinals.com/wp-content/uploads/2020/04/u1Re9qgMfM8d6kumlW85PS6s55jQh5fbdmppgQsP.jpeg";
+  if (time < noon)
+  {
+    image = "https://www.elegantthemes.com/blog/wp-content/uploads/2018/11/how-to-wake-up-early.png";
+    messageText = "Good morning!";
+    color = "black";
+  }
+  else if (time >= evening)
+  {
+    image = "https://static01.nyt.com/images/2020/10/29/style/28MOON-01/oakImage-1603985177355-mediumSquareAt3X.jpg";
+    messageText = "Good evening!";
+    color = "white";
+  }
+  else
+  {
+    image = "https://earthsky.org/upl/2013/09/sunrise-red-sea-Graham-Telford-e1489764712368.jpg";
+    messageText = "Good afternoon!";
+    color = "white";
+  }
 
-  //   let eventMessageJS = document.getElementById("eventMessage");
-  //   let eventImageJS = document.getElementById('eventImage');
-  //   let time1 = [sporttime, wakeuptime, lunchtime, sleeptime, noon, evening];
-  //   let eventMessage1 = ["Let's play sport!","Wake up!","Let's have some lunch!","Have a good sleep!","Good evening","Good afternoon"];
-  //   let eventImage = ["https://wearecardinals.com/wp-content/uploads/2020/04/u1Re9qgMfM8d6kumlW85PS6s55jQh5fbdmppgQsP.jpeg",
-  //   "https://www.elegantthemes.com/blog/wp-content/uploads/2018/11/how-to-wake-up-early.png","https://images-na.ssl-images-amazon.com/images/I/71pJu3A8EML._AC_SL1498_.jpg",
-  //   "https://img.freepik.com/free-vector/little-boy-lying-bed-counting-sheep_74855-7582.jpg?size=338&ext=jpg",
-  //   "https://static01.nyt.com/images/2020/10/29/style/28MOON-01/oakImage-1603985177355-mediumSquareAt3X.jpg",
-  //   "https://earthsky.org/upl/2013/09/sunrise-red-sea-Graham-Telford-e1489764712368.jpg",
-  // ]
-  // let eventMessageColor = ["white","black","black","black","black","white","white",];
-// console.log(eventImage.length);
-// console.log(time1.length);
-//console.log(time1);
-
-// for ( let i = 0; i < time1.length; i++) {
-//    if ( time == time1[i]) {
-//        console.log("ZZZZZZZZZZZZZZZ");
-//          console.log(time);
-//         console.log(time1[i]);
-//         document.getElementById("eventMessage").innerText = eventMessage1;
-//         document.getElementById("eventMessage").innerText = "eventMessage1";
-//        document.getElementById("eventImage").src = eventImage;
-//    }
-//    else {
-//         console.log("YYYYYYYYY");
-//         document.getElementById("eventMessage").innerText = "Good morning";
-//         document.getElementById("eventImage").src = "https://simplifaster.com/wp-content/uploads/2017/05/Sunshine-Vitamin-D.jpg";
-//       }
-// }
-  //eventMessageJS.innerText = messageText;
-  // eventImageJS.src = image;
-  // eventMessageJS.style.color = eventMessageColor;
+  console.log(messageText); 
+  eventMessage.innerText = messageText;
+  eventImage.src = image;
+  eventMessage.style.color = color;
   
-  // showCurrentTime();
+  showCurrentTime();
 };
+updateClock();
 
 let oneSecond = 1000;
-setInterval(updateClock, oneSecond);
+setInterval( updateClock, oneSecond);
 
 function logout() {
 
@@ -108,23 +95,18 @@ function closeForm() {
 let confirm_array = [];
 
 function confirm() { 
-   localStorage.clear;
-    let confirm_timeJS = document.getElementById("appt").value;
-    let confirm_contentJS = document.getElementById("content").value;
-    // alert(typeof(confirm_contentJS));
-    // alert(typeof(confirm_timeJS));
-    let obj = JSON.parse('{"time": "'+ confirm_timeJS +'" , "content": "'+ confirm_contentJS +'" }')
-    confirm_array.push(obj);
-    localStorage.setItem("confirm_arrayls", JSON.stringify(confirm_array));
+  let confirm_timeJS = document.getElementById("appt").value;
+  let confirm_contentJS = document.getElementById("content").value;
+  let obj = JSON.parse('{"time": "'+ confirm_timeJS +'" , "content": "'+ confirm_contentJS +'" }')
+  confirm_array.push(obj);
+  localStorage.setItem("confirm_arrayls", JSON.stringify(confirm_array));
 
 }
 function localStorage1() {
   let eventAddedJS = document.getElementById("eventAdded");
   let confirm_timels = JSON.parse(localStorage.getItem("confirm_arrayls"));
   console.log(confirm_timels);
-  // let confirm_contentls = localStorage.getItem(confirm_contentJS);
-  let addEvent = document.createElement("h2");
-  // console.log(confirm_contentls);
-  addEvent.innerText = confirm_timels;
+  let addEvent = document.createElement("h3");
+  addEvent.innerText = confirm_timels.time;
   eventAddedJS.appendChild(addEvent);
 }
